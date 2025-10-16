f37fb5a32409a8855a01cb91a8a15031a2c0156e
Tristan VALCKE <valcketristan@gmail.com>
2025-10-16 22:25:19 +0200

    refactor(rollup.conf): update banner generation and use figlet instead

--- configs/rollup.conf.js
+++ configs/rollup.conf.js
@@ -18,13 +18,33 @@ const path            = require( 'path' )
 const commonjs        = require( '@rollup/plugin-commonjs' )
 const alias           = require( '@rollup/plugin-alias' )
 const { nodeResolve } = require( '@rollup/plugin-node-resolve' )
-const replace         = require( 'rollup-plugin-re' )
 const terser          = require( 'rollup-plugin-terser' ).terser
+const replace         = require( 'rollup-plugin-re' )
+const figlet          = require( 'figlet' )
 
-function _computeBanner ( name, format ) {
+function getPrettyPackageName() {
 
-    const packageName = name || packageInfos.name
-    let prettyFormat  = ''
+    let packageName = ''
+
+    const nameSplits = packageInfos.name.split( '-' )
+    for ( const nameSplit of nameSplits ) {
+        packageName += nameSplit.charAt( 0 ).toUpperCase() + nameSplit.slice( 1 ) + '.'
+    }
+    packageName = packageName.slice( 0, -1 )
+
+    return packageName
+
+}
+
+function getPrettyPackageVersion() {
+
+    return 'v' + packageInfos.version
+
+}
+
+function getPrettyFormatForBanner( format ) {
+
+    let prettyFormat = ''
 
     switch ( format ) {
 
@@ -49,17 +69,52 @@ function _computeBanner ( name, format ) {
 
     }
 
-    return `console.log('${ packageName } v${ packageInfos.version } - ${ prettyFormat }')`
+    return prettyFormat
+
+}
+
+function _commentarize( banner ) {
+
+    let bannerCommented = '/**\n'
+    bannerCommented += ' * '
+    bannerCommented += banner.replaceAll( '\n', '\n * ' )
+    bannerCommented += '\n'
+    bannerCommented += ` * @desc    ${ packageInfos.description }\n`
+    bannerCommented += ' * @author  [Tristan Valcke]{@link https://github.com/Itee}\n'
+    bannerCommented += ' * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}\n'
+    bannerCommented += ' * \n'
+    bannerCommented += ' */'
+
+    return bannerCommented
+
+}
+
+function _computeBanner( format ) {
+
+    const packageName    = getPrettyPackageName()
+    const packageVersion = getPrettyPackageVersion()
+    const prettyFormat   = getPrettyFormatForBanner( format )
+
+    const figText = figlet.textSync(
+        `${ packageName } ${ packageVersion } - ${ prettyFormat }`,
+        {
+            font:             'Tmplr',
+            horizontalLayout: 'default',
+            verticalLayout:   'default',
+            whitespaceBreak:  true,
+        }
+    )
+
+    return _commentarize( figText )
 
 }
 
 function _computeIntro () {
 
     return '' +
-        'if( iteeValidators === undefined ) { console.error(\'Itee.Core need Itee.Validators to be defined first. Please check your scripts loading order.\') }' + '\n' +
-        'if( iteeUtils === undefined ) { console.error(\'Itee.Core need Itee.Utils to be defined first. Please check your scripts loading order.\') }' + '\n' +
-        'if( threeFull === undefined ) { console.error(\'Itee.Core need Three to be defined first. Please check your scripts loading order.\') }' + '\n' +
-        'if( crypto === undefined ) { console.error(\'Itee.Core need crypto to be defined first !\') }' + '\n'
+        'if( iteeValidators === undefined ) { throw new Error(\'Itee.Core need Itee.Validators to be defined first. Please check your scripts loading order.\') }' + '\n' +
+        'if( iteeUtils === undefined ) { throw new Error(\'Itee.Core need Itee.Utils to be defined first. Please check your scripts loading order.\') }' + '\n' +
+        'if( threeFull === undefined ) { throw new Error(\'Itee.Core need Three to be defined first. Please check your scripts loading order.\') }' + '\n'
 
 }
 
@@ -70,7 +125,7 @@ function _computeIntro () {
  * @param options
  * @return {Array.<json>} An array of rollup configuration
  */
-function CreateRollupConfigs ( options ) {
+function CreateRollupConfigs( options ) {
     'use strict'
 
     const {
@@ -170,7 +225,7 @@ function CreateRollupConfigs ( options ) {
 
                     // advanced options
                     paths:     {},
-                    banner:    isProd ? '' : _computeBanner( name, format ),
+                    banner:    ( isProd ) ? '' : _computeBanner( format ),
                     footer:    '',
                     intro:     ( !isProd && format === 'iife' ) ? _computeIntro() : '',
                     outro:     '',
